02c7f4c8db7467b924719fe0a1093d876503b8ed
Antonia Palacios <mariaantoniapalacios@hotmail.com>
2019-06-21 00:01:42 +0100

    Task-6-hideMarkedItems

--- Todo/wwwroot/js/site.js
+++ Todo/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function toggleIsDoneCheckbox() {
+    var isDone = $("#hideIsDone").prop('checked');
+    $("li").each(function () {
+        if ($(this).html().indexOf('isDone') > -1) {
+            if (isDone) {
+                $(this).hide();
+            }
+            else {
+                $(this).show();
+            }
+        }
+    });
+}
